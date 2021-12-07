c7e3c7e84c9df8eb57bc44e3d17c4b69d877e906
Tom <tombernat7@gmail.com>
2021-12-07 12:46:26 +0100

    Tweet responsive fini

--- my-app/src/Tweet.js
+++ my-app/src/Tweet.js
@@ -39,10 +39,11 @@ class Tweet extends React.Component {
             return (
                 <div className="cont_tweet">
                     <div className="cont_img">
-                    <a href={tweet.url} target="_blank" rel="noreferrer">
-                        <img src={tweet.photo} width="100%" alt="Chargement du drama..."/>
+                    <a  href={tweet.url} target="_blank" rel="noreferrer">
+                        <img src={tweet.photo} alt="Chargement du drama..."/>
                     </a>
                     </div>
+                   
                     <div class="bouton_tweet" >
                         <p class="btn_tweet" style={{fontSize: "1vw"}} onClick={() => this.randomIndex()}>Autre Tweet</p>
                     </div>
